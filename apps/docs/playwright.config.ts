3e05de77afdd423a58f31fc44d4e2fa3705cb873
Andreas <andreas.kutschmann@gmail.com>
2024-11-27 20:52:48 +0100

    prettier fix

--- apps/docs/playwright.config.ts
+++ apps/docs/playwright.config.ts
@@ -72,8 +72,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:4321/',
+    command: "npm run dev",
+    url: "http://localhost:4321/",
     reuseExistingServer: !process.env.CI,
   },
 });
