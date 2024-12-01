0ec7559d710d9e2f3f5b66ced604027bd585631f
Andreas <andreas.kutschmann@gmail.com>
2024-12-01 22:32:41 +0100

    add base url to npm scripts

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://alpinecss.github.io',
-    base: 'alpinecss',
     markdown: {
         syntaxHighlight: false, // 'shiki' | 'prism' | false
         shikiConfig: {
