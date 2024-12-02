f9da2488b786e82b261164208cb43e06f3105f9f
Andreas <andreas.kutschmann@gmail.com>
2024-12-02 13:46:58 +0100

    make playwright report accessible locally

--- apps/docs/visual_regression.sh
+++ apps/docs/visual_regression.sh
@@ -2,5 +2,8 @@ echo "Base URL: $BASE_URL"
 
 docker build -t visual-tests -f Dockerfile.visual-tests --build-arg BASE_URL=$BASE_URL .
 
-docker run --rm --net host -v "$(pwd)/tests:/app/tests" visual-tests
+docker run --rm --net host \
+  -v "$(pwd)/tests:/app/tests" \
+  -v "$(pwd)/playwright-report:/app/playwright-report" \
+  visual-tests
 
