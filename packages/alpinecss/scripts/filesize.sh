3d3118f326b0715748a7265713ad558f3f0016d2
Andreas <andreas.kutschmann@gmail.com>
2024-12-07 23:07:48 +0100

    build fix

--- packages/alpinecss/scripts/filesize.sh
+++ packages/alpinecss/scripts/filesize.sh
@@ -7,7 +7,14 @@
      exit 1
    fi
 
-   FILE_SIZE=$(stat -f%z "$CSS_FILE")
+   if [[ "$OSTYPE" == "darwin"* ]]; then
+     # MacOS
+     FILE_SIZE=$(stat -f%z "$CSS_FILE")
+   else
+     # Linux
+     FILE_SIZE=$(stat --format="%s" "$CSS_FILE")
+   fi
+
    FILE_SIZE_KB=$(echo "scale=2; $FILE_SIZE/1024" | bc)
 
    echo "File size: $FILE_SIZE bytes (${FILE_SIZE_KB} KB)"
